778c0cad5547ce14b38731aed832536c28f0009f
sayyedrabeeh <sayyedrabeeh240@gmail.com>
2024-08-17 17:54:00 +0530

    Add files via upload

---
+++ min-number.py
@@ -0,0 +1,14 @@
+'''
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+'''
+li=[12,34,45,67,7,89]
+min1=li[0]
+for i in li:
+    if i < min1:
+        min1=i
+print(min1)
